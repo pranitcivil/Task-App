d55d5cdc482e5797d4b4fc9bd33b709828830753
pranitcivil <pranitshinde68@gmail.com>
2024-09-22 16:21:12 +0530

    Upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Todo from "./components/Todo";
+
+const App = () => {
+  return (
+    <div className="bg-stone-900 flex justify-center items-center py-4 min-h-screen">
+      <Todo />
+    </div>
+  );
+};
+
+export default App;
